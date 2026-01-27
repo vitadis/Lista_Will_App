d4a491550bee496519f6beb21a01c198ff49ffe3
Joel Mamani <mamanifloresjoel735@gmail.com>
2026-01-27 19:00:07 +0100

    Creacion objeto button

--- Lista_Will_App/src/main/java/org/openjfx/Lista_Will_App/App.java
+++ Lista_Will_App/src/main/java/org/openjfx/Lista_Will_App/App.java
@@ -2,6 +2,7 @@ package org.openjfx.Lista_Will_App;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -18,7 +19,9 @@ public class App extends Application {
         var javafxVersion = SystemInfo.javafxVersion();
 
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Button bb = Nodos_Generales.crearBotonMenu("Hola guapo");
+        
+        var scene = new Scene(new StackPane(bb), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
