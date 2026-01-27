f9548f6058f38cb722d535ccd787782a3507130b
Joel Mamani <mamanifloresjoel735@gmail.com>
2026-01-27 18:02:32 +0100

    Creacion del proyecto javafx con maven

---
+++ Lista_Will_App/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module org.openjfx.Lista_Will_App {
+    requires javafx.controls;
+    exports org.openjfx.Lista_Will_App;
+}
---
+++ Lista_Will_App/src/main/java/org/openjfx/Lista_Will_App/SystemInfo.java
@@ -0,0 +1,13 @@
+package org.openjfx.Lista_Will_App;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
