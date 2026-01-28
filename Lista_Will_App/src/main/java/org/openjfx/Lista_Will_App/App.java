e6fd1a278b91ca384400193e39de24c2d4906f86
Joel Mamani <mamanifloresjoel735@gmail.com>
2026-01-28 14:16:57 +0100

    Metodo de alerta, para confirmar la salida

--- Lista_Will_App/src/main/java/org/openjfx/Lista_Will_App/App.java
+++ Lista_Will_App/src/main/java/org/openjfx/Lista_Will_App/App.java
@@ -2,9 +2,13 @@ package org.openjfx.Lista_Will_App;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.util.Optional;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 /**
@@ -22,8 +26,28 @@ public class App extends Application {
 		var panel2 = GestionVentanas.panel2();
 		var scene = new Scene(panel2, WIDTH, HEIGHT);
 
+		stage.setOnCloseRequest(e -> {
+			e.consume();
+
+			if (confirmarSalida()) {
+				Platform.exit();
+			}
+		});
+
 		stage.setScene(scene);
 		stage.show();
+	}
+
+	// creo un Alert
+	private boolean confirmarSalida() {
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		alert.setTitle("Confirmación");
+		alert.setHeaderText("Cerrar aplicación");
+		alert.setContentText("¿Quieres salir?");
+
+		Optional<ButtonType> result = alert.showAndWait();
+
+		return result.isPresent() && result.get() == ButtonType.OK;
 	}
 
 	public static void main(String[] args) {
